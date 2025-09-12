5085aa4d9f3dab87d5d5ec51538d59a5aa9a38ab
HugoCastilla <hugo.castilla@campus.xaloc.org>
2025-09-12 15:43:48 +0200

    Cambio 1.1
    He añadido un pequeño cuestionario e interacción con el usuario.

--- holamundo Hugo Castilla.py
+++ holamundo Hugo Castilla.py
@@ -1,4 +1,9 @@
 print ("Hola, mundo")
 print ("Mi nombre es PythonChat")
-var1=input ("¿Cómo te llamas?")
-print ("¡ Encantado, "var1"!")
+var1=input ("¿Cómo te llamas? ")
+print ("¡Encantado!,", var1, "!")
+var2=input("¿Tienes alguna pregunta sobre Python?(S/N) ")
+if var2 == "S":
+    print ("¡Estaré encantado de responder cualquier pregunta que tengas!")
+else:
+    print ("Entiendo. ¡Que tenga un buen día!")
