3467a049f9384a7e9fe2a4fba77124aecd9368f2
HugoCastilla <hugo.castilla@campus.xaloc.org>
2025-10-28 10:47:06 +0100

    proyecto password 2

---
+++ Programación Python/UNIDAD 1/PROYECTO PASSWORD/Proyecto Password Hugo Castilla.py
@@ -0,0 +1,21 @@
+print("Bienvenido al programa. Siga las indicaciones para introducir su contraseña:")
+print("")
+print ("1º carácter: Número mayor o igual a 1 o menor o igual a 5.")
+print ("2º carácter: Letra minúscula.")
+print ("3º carácter: Letra mayúscula.")
+print ("4º carácter: Uno de estos símbolos: *, _ , @.")
+print ("5º carácter: Letra minúscula.")
+print ("6º carácter: Número mayor o igual a 6 o menor o igual a 9.")
+print ("7º carácter: Uno de estos símbolos: &, /, #.")
+print ("8º carácter: Un número menor o igual a 5 (No puede ser negativo).")
+
+str(input("Siguiendo las indicaciones, introduzca su contraseña: "))
+
+err1=0
+err2=0
+err3=0
+err4=0
+err5=0
+err6=0
+err7=0
+err8=0
