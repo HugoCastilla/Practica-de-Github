962f0e3f2d8cc6ca8c9321da56dd431017ed79f0
HugoCastilla <hugo.castilla@campus.xaloc.org>
2025-11-04 10:37:17 +0100

    Activitat 4: Ex 35-36

---
+++ Programación Python/UNIDAD 1/ACTIVIDAD 4 - UNIDAD 1/EX. 35.py
@@ -0,0 +1,7 @@
+#Programa que al introducir un número por teclado permita mostrar ese número de veces tu nombre
+
+palabra=input("Introduce una palabra: ")
+num=int(input("Introduce el número de repeticiones: "))
+
+for palabra in range(num):
+    print(palabra)
---
+++ Programación Python/UNIDAD 1/ACTIVIDAD 4 - UNIDAD 1/EX. 36.py
@@ -0,0 +1,9 @@
+#Programa que sume los n primeros números naturales. n lo introduce el usuario.
+
+num=int(input("Introduce un número entero positivo: "))
+
+if num>0:
+    suma = num*(num + 1)//2
+    print(f"La suma de los primeros {num} números naturales es: {suma}")
+else:
+    print("Por favor, introduce un número entero positivo.")
