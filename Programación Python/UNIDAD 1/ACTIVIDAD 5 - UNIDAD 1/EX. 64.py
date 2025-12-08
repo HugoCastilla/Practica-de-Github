c42a0fe39d907986af255e0b638ba79d5ec01abf
HugoCastilla <hugo.castilla@campus.xaloc.org>
2025-12-08 20:17:49 +0100

    Remove comments from EX. 64.py

--- Programación Python/UNIDAD 1/ACTIVIDAD 5 - UNIDAD 1/EX. 64.py
+++ Programación Python/UNIDAD 1/ACTIVIDAD 5 - UNIDAD 1/EX. 64.py
@@ -1,10 +1,2 @@
-# Programa que pida continuamente números por teclado hasta que el usuario introduzca el valor 
-#-99. Será entonces cuando por pantalla aparecerán las siguientes estadísticas:
-#a) total de pares
-#b) total de impares
-#c) total de números positivos
-#d) total de números negativos
-#e) total de ceros
-#f) total de la suma de todos los números introducidos
 
 
