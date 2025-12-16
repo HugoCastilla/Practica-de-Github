0ab259584b0f86a316bf4a81861c63bc5bbfd0ae
HugoCastilla <hugo.castilla@campus.xaloc.org>
2025-12-16 10:29:16 +0100

    PP2 - Acabado

--- Programación Python/UNIDAD 1/PROYECTO PASSWORD/Proyecto Password Hugo Castilla (v2).py
+++ Programación Python/UNIDAD 1/PROYECTO PASSWORD/Proyecto Password Hugo Castilla (v2).py
@@ -6,13 +6,16 @@ uncin=0
 sesnuev=0
 simb=0
 sym="*_@&/#"
+correcto=0
+incorrecto=0
+
 #mostramos por pantalla las indicaciones que el usuario ha de cumplir
 print("Bienvenido al programa. Siga las indicaciones para introducir su contraseña:")
 print("")
-print("Su contraseña debe tener entre 8 o más carácteres.")
-print("")
 while intentos!=3:
     print("Su contraseña debe seguir estas características: ")
+    print("")
+    print("- Su contraseña debe tener entre 8 o más carácteres.")
     print ("- 2 números mayores o iguales a 1 o menores o iguales a 5.")
     print ("- 2 letras minúscula.")
     print ("- Una letra mayúscula.")
@@ -20,7 +23,7 @@ while intentos!=3:
     print ("- Un número mayor o igual a 6 o menor o igual a 9.")
 
     #calculamos la longitud de la contraseña y comprobamos si es correcta
-
+    print("")
     password=str(input("Siguiendo las indicaciones, introduzca su contraseña: "))
     print("")
     intentos=intentos+1
@@ -60,6 +63,9 @@ while intentos!=3:
         if uncin>=2 and sesnuev>=1 and upper>=1 and lower>=2 and simb>=2:
             print("Contraseña correcta.")
             print("")
+            correcto=correcto+1
+        else:
+            incorrecto=incorrecto+1
 
         if intentos==1:
             print(f"Ya ha introducido {intentos} contraseña.")
@@ -71,6 +77,8 @@ while intentos!=3:
         print("")
         print(f"Su contraseña '{password}' es demasiado corta.")
         print("")
+        incorrecto=incorrecto+1
+
         if intentos==1:
             print(f"Ya ha introducido {intentos} contraseña.")
             print("")
@@ -78,10 +86,28 @@ while intentos!=3:
             print(f"Ya ha introducido {intentos} contraseñas.")
             print("")
 
+print(f"Ha introducido {correcto} contraseña/s correctas y {incorrecto} contraseña/s incorrectas.")
+
+
 
 #INTENTOS DE CONTRASEÑAS:
 # - 623MMmm** - Correcta
+
 # - 623Mmm** - Correcta
+
 # - "" - Incorrecta (Menos de 8 caracteres)
+
 # - 99999999999999999iLoP@&13 - Correcta
+
 # - 10Kjp&88 - Incorrecta (Menos de dos números entre 1 y 5)(Menos de 2 símbolos)
+
+# - **ççÇ1.2//9mmjjkhafksghefjkvnhuigyufghekjghreuifgeyishgkgyiwyeyü - Correcta
+
+# - 1234567890qwertyuiopasdfghjklñçzxcvbnm!@#$%&/() - Correcta
+
+# - 23tT_8 - Incorrecta (Menos de 8 caracteres)
+
+# - Castilla_La_Mancha7/34 - Correcta
+
+# - x2ge7cv4c27r$%&% - Incorrecta (Menos de 2 mayúsculas)(Menos de dos símbolos)
+
