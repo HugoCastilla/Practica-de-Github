df0bce25bb13565ce8271f35b0ba87f81b7af250
HugoCastilla <hugo.castilla@campus.xaloc.org>
2026-01-23 18:11:54 +0100

    change of skibidi

--- Programación Python/UNIDAD 2/ACTIVIDAD 2/EX. 70.py
+++ Programación Python/UNIDAD 2/ACTIVIDAD 2/EX. 70.py
@@ -2,9 +2,14 @@
 
 num=int(input("Introduce una cantidad de palabras: "))
 
-lista=[]
+lista1=[]
+lista2=[]
 
 for i in range(num):
-    lista.append(input("Introduce una palabra: "))
+    palabra=input("Introduce una palabra: ")
+    lista1.append(palabra)
+    lista2.append(palabra)
 
-print(list(set(lista)))
+lista2.reverse()
+print(lista1)
+print(lista2)
