db897fd957cb3608c440f28a9c3e09e9097db28e
HugoCastilla <hugo.castilla@campus.xaloc.org>
2026-01-28 09:54:43 +0100

    mejora del dni (casi acabado)

--- Programación Python/TRISTÁN 1.0.1/Tristán 1.0.1.py
+++ Programación Python/TRISTÁN 1.0.1/Tristán 1.0.1.py
@@ -1,36 +1,51 @@
 import random
 quier1=0
-
+listahorcado=["plátano", "niño", "balón", "comedor", "calcetín", "padre", "#!@~/€%&*^!", "agua", "café", "aigua", "zumo", "autismo", "enfermeda", "espacio", "zarigüeya"]
+ahorca=""
+letra=""
+juego2="s"
 print("Hola, mi nombre es Tristán 1.0.1")
 name=str(input("¿Cual es tu nombre?  "))
 print("")
 print(f"Encantado, {name}.")
-juego1=str(input("¿Quieres que juguemos un juego? (S/N) "))
+juego1=str(input("¿Quieres que juguemos un juego? (s/n) ")).lower()
 
 num=random.randint(1,10)
-while quier1!="N":
-    if juego1=="S":
+while quier1!="n":
+    if juego1=="s":
         guess=int(input("Intenta adivinar el número del 1 al 10: "))
         if num==guess:
             print("")
             print("¡Has acertado el número!")
             break
         else:
-            quier1=str(input("Incorrecto. ¿Quieres probar otra vez? (S/N) "))
-            if quier1=="N":
+            quier1=str(input("Incorrecto. ¿Quieres probar otra vez? (s/n) ")).lower()
+            if quier1=="n":
                 break
-            elif quier1=="S":
-                quier1="S"
+            elif quier1=="s":
+                quier1="s"
     else:
         break
 print("")
 print("Bueno, se me ocurre otra cosa que podemos hacer.") 
-juego2=str(input("¿Quieres jugar al ahorcado? (S/N) "))
-if juego2=="S":
-    print("hi")
-elif juego2=="N":
-    print("Niggers")
+juego2=str(input("¿Quieres jugar al ahorcado? (s/n) ")).lower()
+if juego2=="s":
+    while juego2!="s":
+        ahorcado=random.choice(listahorcado)
+        for i in len(ahorcado):
+            ahorca=ahorca+" _ "
+        print(ahorca)
+        print(ahorcado)
+        letra=str(input("Introduce una letra: "))
 else:
-    print("Repite porfa")
+    if juego1 and juego2=="n":
+        print("Vaya. Parece que hoy no tenemos ganas de jugar, ¿eh?")
+    else:
+        print("")
+print("Déjame un momento para que piense lo que podemos hacer ahora.")
+
+
+
+    
 
 
--- Programación Python/UNIDAD 2/DNI.py
+++ Programación Python/UNIDAD 2/DNI.py
@@ -2,7 +2,11 @@
 
 #definimos las letras en el orden establecido
 letras=["T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"]
-
+totcor=0
+toterr=0
+long=0
+malnum=0
+maletra=0
 intentos=[]
 dnibien=[]
 dnimal=[]
@@ -10,14 +14,16 @@ seguir="s"
 opcion=""
 #hacemos que el bucle se repita hasta que el usuario diga "n"
 while seguir!="n":
-    dni = input("Introduce un DNI: ")
-    if len(dni) !=8:
+    dni=input("Introduce un DNI: ")
+    if len(dni)!=8:
         print("Error: longitud incorrecta")
-        intentos.append(0)
+        long=long+1
+        toterr=toterr+1
         dnimal.append(dni)
     elif not dni.isdigit():
         print("Error: no es numerico")
-        intentos.append(1)
+        maletra=maletra+1
+        toterr=toterr+1
         dnimal.append(dni)
     else:
         #calculamos la letra del dni dependiendo del residuo de la división
@@ -25,18 +31,20 @@ while seguir!="n":
         resto=numero%23
         if resto<0 or resto>22:
             print("Error: resto no valido")
-            intentos.append(2)
+            malnum=malnum+1
+            toterr=toterr+1
             dnimal.append(dni)
         else:
             letra=letras[resto]
             print("DNI correcto:", dni+"-"+letra)
-            intentos.append(3)
-            dnibien.append(dni)
+            totcor=totcor+1
+            dnibien.append(dni+"-"+letra)
     #ponemos lower para que si introduce la "n" mayúscula cuente también
     seguir=input("¿Quieres introducir otro DNI? (s/n): ").lower()
 
 while opcion !="6":
-    #mostramos las opciones posibles
+    #mostramos las opciones posibles, uso prints vacios para dejar un poco de espacio en la salida y que quede mejor visualmente
+    print("")
     print("MENÚ")
     print("1. Listar DNIs correctos")
     print("2. Listar DNIs incorrectos")
@@ -46,29 +54,40 @@ while opcion !="6":
     print("6. Salir")
 
     opcion=input("Elige una opción: ")
+    print("")
     if opcion=="1":
-        dnibien.sort()
-        print("DNIs correctos:")
-        for d in dnibien:
-            print(d)
+        if totcor!=0:
+            dnibien.sort()
+            print("DNIs correctos:")
+            print(dnibien)
+        else:
+            print("No se han introducido DNIs correctos")
     elif opcion=="2":
-        dnimal.sort()
-        print("DNIs incorrectos:")
-        for d in dnimal:
-            print(d)
+        if toterr!=0:
+            dnimal.sort()
+            print("DNIs incorrectos:")
+            print(dnimal)
+        else:
+            print("No se han introducido DNIs incorrectos")
     elif opcion=="3":
-        toterr=intentos.count(0)+intentos.count(1)+intentos.count(2)
         print("Número total de errores:", toterr)
     elif opcion=="4":
-        totcor=intentos.count(3)
         print("Número total de DNIs correctos:", totcor)
     elif opcion=="5":
-        total=intentos.count(0)+intentos.count(1)+intentos.count(2)+intentos.count(3)
+        total=toterr+totcor
         if total==0:
             print("No se han ingresado DNIs.")
         else:
-            porcentaje=(totcor/total)*100
+            porcentaje=round((totcor/total)*100, 1)
             print("Porcentaje de DNIs correctos:",porcentaje)
+            porcentmalje=round(100-porcentaje, 1)
+            print("Porcentaje de DNIs incorrectos:",porcentmalje)
+            porcentlong=round((long/total)*100, 1)
+            print("Porcentaje de DNIs con longitud incorrecta:",porcentlong)
+            porcenmalnum=round((malnum/total)*100,1 )
+            print("Porcentaje de DNIs con error de dígitos:",porcenmalnum)
+            porcenletra=round((maletra/total)*100, 1)
+            print("Porcentaje de DNIs que no existen:",porcenletra)
     elif opcion=="6":
         print("Fin del programa.")
     else:
