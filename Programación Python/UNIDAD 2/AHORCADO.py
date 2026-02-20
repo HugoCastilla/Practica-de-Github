0ba6051564249751c04b5992a1f98f801133e9d0
HugoCastilla <hugo.castilla@campus.xaloc.org>
2026-02-20 16:02:37 +0100

    Update AHORCADO.py

--- Programación Python/UNIDAD 2/AHORCADO.py
+++ Programación Python/UNIDAD 2/AHORCADO.py
@@ -3,15 +3,21 @@ import time
 
 lista_partida=[]
 lista_palabrasecreta=[]
-lisa_ahorcado=[]
+lista_ahorcado=[]
+errores=0
+palabra=""
 
 seguir="s"
 print("Bienvenido al juego del ahorcado")
 seguir=input("Quieres echar una partida? (s/n): ").lower()
 
 while seguir=="s":
-    lista_palabrasecreta.append(random.choice(["guatemalteco","perro","gato","elefante","jirafa","tigre", "leon","mono","pajaro","pez","serpiente", "conejo","tortuga","cocodrilo","hipopotamo","rinoceronte","zebra","cebra","camello","delfin","ballena", "tiburon","pulpo","medusa","estrella de mar","caracol","mariposa","abeja","hormiga","araña","mosquito","mosca","mariposa nocturna", "libelula","grillo","saltamontes","cucaracha","termita","avispa","escarabajo","gallina","pato","pavo","ganso","pinguino","flamenco","cisne","aguila","halcon","buitre", "lechuza", "buho", "paloma", "cuervo", "gorrión", "loro", "canario", "periquito", "cotorra", "agapornis", "cacatua", "guacamayo", "tucan", "colibri", "cardenal", "azulejo", "verderon", "jilguero", "mirlo", "petirrojo", "ruiseñor", "zorzal", "tordo", "alondra", "avioneta", "helicoptero", "globo aerostatico", "paracaidas", "planeador", "drone", "cohete", "satélite", "estacion espacial", "ovni", "nave espacial", "astronauta","marte","venus","jupiter","saturno","urano","neptuno","pluton", "sol","luna","estrella","galaxia","universo","cometa","asteroide","meteorito", "constelacion", "supernova", "agujero", "negro", "blanco", "rojo", "azul", "verde", "amarillo", "naranja", "morado", "rosa", "gris", "marron", "violeta", "celeste", "turquesa", "fucsia", "lila", "beige", "dorado", "plateado","cielo","mar","montaña","río","bosque","desierto","selva","pradera","isla","océano","laguna","cascada","volcán","glaciar","cueva","valle","colina","playa","arena","roca","piedra","árbol","flor","hierba","hoja","fruto","semilla", "raíz", "tallo", "rama", "tronco", "corteza", "flor", "fruto", "semilla", "hoja", "hierba", "césped", "maleza", "arbusto", "seto", "jardín", "parque", "bosque", "selva","desierto","pradera","isla","océano","laguna","cascada","volcán","glaciar","cueva","valle","colina","playa","arena","roca","piedra"]))
-    palabra=lista_palabrasecreta[0].upper()
+    lista_palabrasecreta.append(random.choice(["guatemalteco","perro","gato","elefante","jirafa","tigre","mono","pez","serpiente", "conejo","tortuga","cocodrilo","rinoceronte","cebra","camello","ballena","pulpo","medusa","caracol","mariposa","abeja","hormiga","araña","mosquito","mosca","grillo","saltamontes","cucaracha","termita","avispa","escarabajo","gallina","pato","pavo","ganso","flamenco","cisne","buitre", "lechuza", "paloma", "cuervo", "loro", "canario", "periquito", "cotorra", "agapornis", "guacamayo", "cardenal", "azulejo", "jilguero", "mirlo", "petirrojo", "ruiseñor", "zorzal", "tordo", "alondra", "avioneta", "planeador", "cohete", "ovni", "astronauta","marte","venus","saturno","urano","neptuno","pluton", "sol","luna","estrella","galaxia","universo","cometa","meteorito", "supernova", "agujero", "negro", "blanco", "rojo", "azul", "verde", "amarillo", "naranja", "morado", "rosa", "gris", "marron", "violeta", "celeste", "turquesa", "fucsia", "lila", "beige", "dorado", "plateado","cielo","mar","montaña","bosque","desierto","selva","pradera","isla","laguna","cascada","glaciar","cueva","valle","colina","playa","arena","roca","piedra","flor","hierba","hoja","fruto","semilla", "tallo", "rama", "tronco", "corteza", "flor", "fruto", "semilla", "hoja", "hierba", "maleza", "arbusto", "seto", "parque", "bosque", "selva","desierto","pradera","isla","laguna","cascada","glaciar","cueva","valle","colina","playa","arena","roca","piedra"]))
+    palabra=str(lista_palabrasecreta[0].upper())
+    iniciotiem = time.perf_counter()
+    totaltiem = 0
+    for i in range(1_000_000):
+        totaltiem += i
     for i in range(len(palabra)):
         lista_partida.append("_")
     while "_" in lista_partida:
@@ -20,7 +26,7 @@ while seguir=="s":
         print("")
         if len(respuesta)==1:
             if respuesta==palabra:
-                print((f"¡Has acertado la palabra!"))
+                print((f"¡Has acertado la palabra: {palabra}!"))
                 lista_partida=[]
                 seguir=input("¿Quieres echar otra partida? (s/n): ").lower()
                 if seguir!="s":
@@ -40,9 +46,49 @@ while seguir=="s":
                                 lista_partida[i]=respuesta
                     else:
                         print((f"¡La letra {respuesta} no está en la palabra!"))
+                        if lista_ahorcado==[]:
+                            lista_ahorcado.append("A")
+                            print(lista_ahorcado)
+                        elif lista_ahorcado==["A"]:
+                            lista_ahorcado.append("H")
+                            print(lista_ahorcado)
+                        elif lista_ahorcado==["A","H"]:
+                            lista_ahorcado.append("O")
+                            print(lista_ahorcado)
+                        elif lista_ahorcado==["A","H","O"]:
+                            lista_ahorcado.append("R")
+                            print(lista_ahorcado)
+                        elif lista_ahorcado==["A","H","O","R"]:
+                            lista_ahorcado.append("C")
+                            print(lista_ahorcado)
+                        elif lista_ahorcado==["A","H","O","R","C"]:
+                            lista_ahorcado.append("A")
+                            print(lista_ahorcado)
+                        elif lista_ahorcado==["A","H","O","R","C","A"]:
+                            lista_ahorcado.append("D")
+                            print(lista_ahorcado)
+                        elif lista_ahorcado==["A","H","O","R","C","A","D"]:
+                            lista_ahorcado.append("O")
+                            print(lista_ahorcado)
+                            print(f"¡Has perdido! La palabra era: {palabra}")
+                            fintiem=time.perf_counter()
+                            tiempo=round(fintiem-iniciotiem,2)
+                            print(f"Tiempo de juego: {tiempo:.6f} segundos")
+                            lista_partida=[]
+                            seguir=input("¿Quieres echar otra partida? (s/n): ").lower()
+                            if seguir!="s":
+                                break
+                            else:
+                                lista_palabrasecreta=[]
+                                lista_partida=[]
+                                lista_ahorcado=[]
+                                break
         else:
             if respuesta==palabra:
-                print((f"¡Has acertado la palabra!"))
+                print((f"¡Has acertado la palabra: {palabra}!"))
+                fintiem=time.perf_counter()
+                tiempo=round(fintiem-iniciotiem,2)
+                print(f"Tiempo de juego: {tiempo:.6f} segundos")
                 lista_partida=[]
                 seguir=input("¿Quieres echar otra partida? (s/n): ").lower()
                 if seguir!="s":
@@ -50,5 +96,48 @@ while seguir=="s":
                 else:
                     lista_palabrasecreta=[]
                     lista_partida=[]
+                    lista_ahorcado=[]
                     break
-print("¡Gracias por jugar!")
+            else:
+                print((f"¡La palabra {respuesta} no es correcta!"))
+                if lista_ahorcado==[]:
+                    lista_ahorcado.append("A")
+                    print(lista_ahorcado)
+                elif lista_ahorcado==["A"]:
+                    lista_ahorcado.append("H")
+                    print(lista_ahorcado)
+                elif lista_ahorcado==["A","H"]:
+                    lista_ahorcado.append("O")
+                    print(lista_ahorcado)
+                elif lista_ahorcado==["A","H","O"]:
+                    lista_ahorcado.append("R")
+                    print(lista_ahorcado)
+                elif lista_ahorcado==["A","H","O","R"]:
+                    lista_ahorcado.append("C")
+                    print(lista_ahorcado)
+                elif lista_ahorcado==["A","H","O","R","C"]:
+                    lista_ahorcado.append("A")
+                    print(lista_ahorcado)
+                elif lista_ahorcado==["A","H","O","R","C","A"]:
+                    lista_ahorcado.append("D")
+                    print(lista_ahorcado)
+                elif lista_ahorcado==["A","H","O","R","C","A","D"]:
+                    lista_ahorcado.append("O")
+                    print(lista_ahorcado)
+                    print(f"¡Has perdido! La palabra era: {palabra}")
+                    fintiem=time.perf_counter()
+                    tiempo=round(fintiem-iniciotiem,2)
+                    print(f"Tiempo de juego: {tiempo:.6f} segundos")
+                    lista_partida=[]
+                    seguir=input("¿Quieres echar otra partida? (s/n): ").lower()
+                    if seguir!="s":
+                        break
+                    else:
+                        lista_palabrasecreta=[]
+                        lista_partida=[]
+                        lista_ahorcado=[]
+                        break
+if palabra=="":
+    print("¡Está bien, ya jugaremos otro día!")
+else:
+    print("¡Gracias por jugar!")
